7bd582a44630008d34f9bef5a8a2b3980fb54097
RocklightZL <3454448283@qq.com>
2025-07-06 21:00:31 +0800

    draw the first triangle

--- Cube/src/Cube/Core/Application.cpp
+++ Cube/src/Cube/Core/Application.cpp
@@ -17,9 +17,30 @@ namespace Cube {
         running = true;
         init();
         CB_CORE_INFO("Application run");
+
+        unsigned int VBO;
+        float vertices[3*3] {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f, 0.5f, 0.0f
+        };
+        glGenBuffers(1, &VBO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+        glEnableVertexAttribArray(0);
+
+        unsigned int EBO;
+        unsigned int indices[]{0, 1, 2};
+        glGenBuffers(1, &EBO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
         while(running) {
             glClearColor(1.0f, 0.75f, 0.8f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
+
+            glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
 
             mainWindow->update();
         }
